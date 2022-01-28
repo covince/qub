7b9e49785f4420a51549730392198f2d57361ee5
Richard Goater <rg19@sanger.ac.uk>
2022-01-28 15:38:28 +0000

    Update footer

--- ui/src/components/Footer.jsx
+++ ui/src/components/Footer.jsx
@@ -6,15 +6,12 @@ import { BsEnvelope } from 'react-icons/bs'
 import Button from 'covince/src/components/Button'
 
 import COGUKLogo from './COGUKLogo'
-import SangerLogo from '../assets/logo-sanger-full-colour.svg'
-// import UKHSALogoLarge from '../assets/ukhsa-large.jpg'
-
-// import { version, lastUpdated } from '../version'
+import PHALogo from '../assets/PHAlogo.png'
 
 const Footer = () => (
   <footer className='qub-footer bg-white dark:bg-gray-700 mt-2 md:mt-0 border-t border-gray-100 dark:border-gray-500 shadow text-heading dark:!text-dark-heading space-y-6 py-6 md:py-9'>
-    <div className='container px-4 md:px-12 grid grid-cols-1 gap-9 md:grid-cols-2 lg:grid-cols-3 lg:gap-6 xl:gap-0'>
-      <div className='space-y-3 max-w-md'>
+    <div className='container px-4 md:px-12 grid grid-cols-1 gap-9 lg:grid-cols-3 lg:gap-6 xl:gap-0'>
+      <div className='space-y-3 md:max-w-lg'>
         <form>
           <Button
             as='a'
@@ -26,16 +23,11 @@ const Footer = () => (
           </Button>
         </form>
         <p className='text-sm'>
-          This page uses the <a href='https://github.com/covince'>CovInce</a> visualisation framework, developed as part of the <a href='https://www.sanger.ac.uk/science/covid-19-science/'>Wellcome Sanger Institute <span className='whitespace-nowrap'>COVID–19</span>&nbsp;Genomics Initiative</a>.
+          Site hosted by <strong>Centre for Geographical Information Science and Geomatics, Geography, School Natural and Built Environment, QUB</strong>. Developed with the <a href='https://www.sanger.ac.uk/science/covid-19-science/'>Wellcome Sanger Institute <span className='whitespace-nowrap'>COVID–19</span>&nbsp;Genomics Initiative</a>.
         </p>
-        {/* <p className='text-sm'>
-          This work is funded by <strong>The Department for Health and Social Care Testing Innovation Fund</strong> and <strong>Wellcome</strong>
-        </p> */}
       </div>
       <div className='grid gap-12 my-3 sm:my-0 pl-4 sm:pl-0 sm:justify-evenly sm:flex sm:items-center md:col-span-2'>
-        <a href='https://sanger.ac.uk'>
-          <img src={SangerLogo} className='w-52' />
-        </a>
+        <img src={PHALogo} className='w-64' />
         <a href='https://www.cogconsortium.uk'>
           <COGUKLogo className='w-64' />
         </a>
@@ -47,9 +39,6 @@ const Footer = () => (
           {/* <li>Contains UK Health Security Agency data © Crown copyright and database right 2020</li> */}
           <li>Office for National Statistics licensed under the Open Government Licence v.3.0</li>
         </ul>
-        {/* <p className='leading-6 pt-6 pb-18 md:pb-6 text-sm w-content'>
-          &copy; 2021 <a href='https://sanger.ac.uk' className='underline hover:no-underline'>Wellcome Sanger Institute</a>
-        </p> */}
       </div>
     </div>
   </footer>
